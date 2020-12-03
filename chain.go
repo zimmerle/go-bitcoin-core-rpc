9a45d7e5bcab6f08eca1c00ca19a8981e801fa81
Felipe Zimmerle <felipe@zimmerle.org>
2020-12-03 12:27:16 -0300

    aoeuaoeu

--- chain.go
+++ chain.go
@@ -206,7 +206,8 @@ func (c *Client) GetBlockVerboseTxAsync(blockHash *chainhash.Hash) FutureGetBloc
 	}
 
 	cmd := btcjson.NewGetBlockCmd(hash, btcjson.Bool(true), btcjson.Bool(true))
-	fmt.Println(*cmd)
+	fmt.Println("**** cmd: ")
+	fmt.Printf("%#v", *cmd)
 	return c.sendCmd(cmd)
 }
 
